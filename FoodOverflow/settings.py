05defd00e507401a269664081868ce3432725af8
Jh0mpis <jmorenotr@unal.edu.co>
2024-03-06 20:33:19 -0500

    [34] Adding rest settings

--- FoodOverflow/settings.py
+++ FoodOverflow/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "FoodOverflowApp",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -129,4 +129,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+#Cors authorization
 CORS_ALLOW_ALL_ORIGINS = True
