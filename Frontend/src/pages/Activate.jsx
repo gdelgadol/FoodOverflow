4450492546143d33ed5a794dd0795771177116dd
Juan Ovalle <jpovalleg@gmail.com>
2024-03-16 21:45:19 -0500

    Activar cuenta Front

--- Frontend/src/pages/Activate.jsx
+++ Frontend/src/pages/Activate.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import { useParams } from 'react-router-dom'; // Asumiendo que estás utilizando React Router
-
+import React, {useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import axios from "../api/axios.jsx";
+import { useNavigate } from "react-router-dom";
 function Activate() {
   const { uid, token } = useParams(); // Obtener los parámetros de la URL
+  const navigate = useNavigate();
+
+  const activate = () => {
+    axios
+      .post(`http://127.0.0.1:8000/activate/${uid}/${token}`, {
+      })
+      .then((res) => {
+          navigate("/");
+          alert(res.data.message);
+      });
+  };
+
+  useEffect(() => {activate()}, [])
 
   return (
-    <div>
-      <p>Usuario: {uid}</p>
-      <p>Token: {token}</p>
-    </div>
+    <></>
   );
 }
 
