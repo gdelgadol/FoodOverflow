fd158f8b912debc4c84b9d0c1d1968065aad0c5f
Juan Ovalle <jpovalleg@gmail.com>
2024-04-22 18:23:34 -0500

    URL de la DB desplegada en render

--- FoodOverflow/settings.py
+++ FoodOverflow/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config, Csv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,6 +97,8 @@ DATABASES = {
         "PORT": config('DB_PORT'),
     }
 }
+
+DATABASES['default'] = dj_database_url.parse(config('DB_URL'))
 
 
 # Password validation
