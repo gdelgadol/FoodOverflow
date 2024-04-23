ad28937215699eae124e2456257a1657f4df82fd
Jh0mpis <jmorenotr@unal.edu.co>
2024-04-23 09:39:45 -0500

    Backend DataBase problems fixed

--- FoodOverflowApp/admin.py
+++ FoodOverflowApp/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Profile
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(Publication)
+admin.site.register(PublicationComment)
+admin.site.register(PublicationVote)
+admin.site.register(Recipe)
+admin.site.register(RecipeComment)
+admin.site.register(RecipeVote)
