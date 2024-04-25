7f536a444f99d3b13fb81ed744ace02dcc13b862
CamiloArdila <anardilad@unal.edu.co>
2024-04-24 20:06:10 -0500

    Email change

--- Frontend/src/pages/DetallesPublicacion.jsx
+++ Frontend/src/pages/DetallesPublicacion.jsx
@@ -155,11 +155,19 @@ function DetallesPublicacion() {
     <div className="dp-container">
       <div className="dp-post">
         <div className="dp-score">
-          <button className={`vote-button ${voted && lastVote === 1 ? 'voted' : ''} ${voteStatus === 1 ? 'user-voted' : ''}`} onClick={() => handleVote(1)}>
+          <button
+            className={`vote-button ${voted && lastVote === 1 ? 'voted' : ''} ${voteStatus === 1 ? 'user-voted' : ''}`}
+            onClick={() => handleVote(1)}
+            title="Estás de acuerdo en que es información útil, relevante o correcta"
+          >
             <HiArrowCircleUp size={30} />
           </button>
           {score}
-          <button className={`vote-button ${voted && lastVote === -1 ? 'voted' : ''} ${voteStatus === -1 ? 'user-voted' : ''}`} onClick={() => handleVote(-1)}>
+          <button
+            className={`vote-button ${voted && lastVote === -1 ? 'voted' : ''} ${voteStatus === -1 ? 'user-voted' : ''}`}
+            onClick={() => handleVote(-1)}
+            title="Consideras que la información no es útil, relevante o correcta"
+          >
             <HiArrowCircleDown size={30} />
           </button>
         </div>
