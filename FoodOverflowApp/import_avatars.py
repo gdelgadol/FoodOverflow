888b1bc671ba91bffe44c5abbf2ab23849067a8d
Jh0mpis <jmorenotr@unal.edu.co>
2024-05-01 17:40:09 -0500

    [114] Fixing the database populating

--- FoodOverflowApp/import_avatars.py
+++ FoodOverflowApp/import_avatars.py
@@ -1,14 +1,19 @@
 import csv
 from .models import Avatar
 
-def import_avatars(file_path):
-    with open(file_path, 'r') as file:
+def run():
+    with open('FoodOverflowApp/avatars.csv', 'r') as file:
         reader = csv.DictReader(file)
+        counter_exists = 0
+        counter_new = 0
         for row in reader:
             if not Avatar.objects.filter(avatar_url = row['avatar_url']).exists():
                 Avatar.objects.create_avatar(
                     url = row['avatar_url']
                 )
                 print(row['avatar_url']+" avatar created.")
-
-import_avatars('FoodOverflowApp/avatars.csv')
+                counter_new += 1
+            else:
+                counter_exists += 1
+        print(str(counter_exists)+" avatars exists before.")
+        print(str(counter_new)+" avarars added to the model.")
