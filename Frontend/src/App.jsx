7fa156b78d0f6bb5232acf773fe7cbd4d191a3f8
CamiloArdila <anardilad@unal.edu.co>
2024-05-08 15:43:42 -0500

    [54] Tags for the search bar DONE

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,6 +22,8 @@ import SupportUs from "./pages/SupportUs.jsx"
 import UserProfile from "./pages/UserProfile.jsx"
 import Terms from "./pages/Terms.jsx"
 import Acuerdo from "./pages/Acuerdo.jsx"
+import Busqueda from "./components/Busqueda.jsx"
+import ResultsSearch from "./pages/Results_search.jsx"
 
 function App() {
   return (
@@ -49,6 +51,8 @@ function App() {
           <Route path="/support" element={<SupportUs />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/acuerdo" element={<Acuerdo />} />
+          <Route path="/search/:tags" element={<Busqueda />} />
+          <Route path="/results_search/:tags" element={<ResultsSearch />} />
           <Route path="/user/:username" element={<UserProfile />} />
         </Routes>
       </div>
--- Frontend/src/components/Encabezado.jsx
+++ Frontend/src/components/Encabezado.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
-import "./Encabezado.css";
-import iconoImg from "../assets/logo.png";
-import { Link } from "react-router-dom";
+import axios from 'axios';
+import './Encabezado.css';
+import iconoImg from '../assets/logo.png';
+import { Link, useNavigate } from 'react-router-dom'; // Importamos useNavigate
 import Cookies from 'universal-cookie';
+import Creatable from 'react-select/creatable';
 
 function Encabezado() {
   const cookies = new Cookies();
-  const jwt = cookies.get("auth_token");
+  const jwt = cookies.get('auth_token');
+  const navigate = useNavigate(); // Usamos useNavigate para la navegación
 
   const [menuOpen, setMenuOpen] = useState(false);
   const [menuUsuarioOpen, setMenuUsuarioOpen] = useState(false);
-  const urlFront =  import.meta.env.VITE_FRONT_URL;
+  const [selectedTags, setSelectedTags] = useState([]);
+  const urlFront = import.meta.env.VITE_FRONT_URL;
+  const url = import.meta.env.VITE_API_URL;
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -21,73 +26,120 @@ function Encabezado() {
   };
 
   const handleLogout = () => {
-    // Eliminar el token JWT
     cookies.remove('auth_token', { path: '/' });
     setTimeout(() => {
       window.location.href = `${urlFront}/login`;
     }, 500);
   };
 
+  const tagsDictionary = [
+    { label: 'Vegetariano', value: 1 },
+    { label: 'Vegano', value: 2 },
+    { label: 'Sin gluten', value: 3 },
+    { label: 'Bajo en carbohidratos', value: 4 },
+    { label: 'Alta en proteínas', value: 5 },
+    { label: 'Postre', value: 6 },
+    { label: 'Desayuno', value: 7 },
+    { label: 'Almuerzo', value: 8 },
+    { label: 'Cena', value: 9 },
+    { label: 'Aperitivo', value: 10 }
+  ];
+
+  const customStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      width: '25vw',
+      borderColor: '#ccc',
+      '&:focus': {
+        borderColor: '#ff5c00',
+      },
+    }),
+
+    placeholder: (provided, state) => ({
+      ...provided,
+      marginLeft: '10px',
+    }),
+  };
+
+  const submitSearch = async () => {
+    try {
+      const selectedValues = selectedTags.map(tag => tag.value);
+      const tagsParam = selectedValues.join(',');
+      navigate(`/results_search/${tagsParam}`); // Navegamos a la ruta de búsqueda con los tags en la URL
+    } catch (error) {
+      console.error('Error al realizar la búsqueda:', error);
+    }
+  };
+
   return (
     <div className="header">
-      {/* Contenido para tamaños de pantalla más grandes */}
       <button className="menu-toggle" onClick={toggleMenu}>
         <div></div>
         <div></div>
         <div></div>
       </button>
-      <Link to={"/"}>
-      <div className="icon-header">
-        <img
-          src={iconoImg}
-          alt="Icono de la página"
-          style={{ width: "37px", height: "43px" }}
-        />
-        <div className="logo">FoodOverflow</div>
-      </div>
+      <Link to={'/'}>
+        <div className="icon-header">
+          <img
+            src={iconoImg}
+            alt="Icono de la página"
+            style={{ width: '37px', height: '43px' }}
+          />
+          <div className="logo">FoodOverflow</div>
+        </div>
       </Link>
       <div className="search">
-        <input type="text" placeholder="Buscar" />
+        <Creatable
+          className="search-select"
+          placeholder="Buscar"
+          options={tagsDictionary}
+          isMulti
+          value={selectedTags}
+          onChange={selectedOptions => setSelectedTags(selectedOptions)}
+          styles={customStyles}
+        />
+        <button className="search-button" onClick={submitSearch}>
+          <strong>Buscar</strong>
+        </button>
       </div>
       <div className="menu">
         <div className="menu-info">
-        <Link to={"/"}>
-        <button className="menu-button">
-          <span className="spanHeader">Inicio</span>
-        </button>
-        </Link>
-        <Link to={"/about"}>
-        <button className="menu-button">
-        <span className="spanHeader">Sobre nosotros</span>
-        </button>
-        </Link>
-        <Link to={"/support"}>
-        <button className="menu-button">
-          <span className="spanHeader">Apóyanos</span>
-        </button>
-        </Link>
-        <div className="separator"></div>
+          <Link to={'/'}>
+            <button className="menu-button">
+              <span className="spanHeader">Inicio</span>
+            </button>
+          </Link>
+          <Link to={'/about'}>
+            <button className="menu-button">
+              <span className="spanHeader">Sobre nosotros</span>
+            </button>
+          </Link>
+          <Link to={'/support'}>
+            <button className="menu-button">
+              <span className="spanHeader">Apóyanos</span>
+            </button>
+          </Link>
+          <div className="separator"></div>
         </div>
-        {/* Botones para tamaños de pantalla más pequeños */}
         {jwt ? (
           <>
-            <Link to={"/crear_publicacion"}>
+            <Link to={'/crear_publicacion'}>
               <button className="menu-button2">
-                <strong>Publicar</strong> 
+                <strong>Publicar</strong>
               </button>
             </Link>
-              <button className="menu-button3" onClick={toggleMenuUsuario}>
-                <strong>Usuario</strong>
-              </button>
+            <button className="menu-button3" onClick={toggleMenuUsuario}>
+              <strong>Usuario</strong>
+            </button>
           </>
         ) : (
           <>
-            <Link to={"/login"}>
+            <Link to={'/login'}>
               <button className="menu-button2">
-                <strong>Iniciar sesión</strong> 
+                <strong>Iniciar sesión</strong>
               </button>
             </Link>
-            <Link to={"/register"}>
+            <Link to={'/register'}>
               <button className="menu-button3">
                 <strong>Registrarse</strong>
               </button>
@@ -95,19 +147,18 @@ function Encabezado() {
           </>
         )}
       </div>
-      
-      {/* Menú de usuario */}
+
       {menuUsuarioOpen && (
         <div className="menu-usuario">
-          <Link to={"/profile"}>
-          <button className="menu-button-user" onClick={toggleMenuUsuario}>
-            <span className="spanHeader">Configuración de la cuenta</span>
-          </button>
+          <Link to={'/profile'}>
+            <button className="menu-button-user" onClick={toggleMenuUsuario}>
+              <span className="spanHeader">Configuración de la cuenta</span>
+            </button>
           </Link>
-          <Link to={"/user/profile"}>
-          <button className="menu-button-user" onClick={toggleMenuUsuario}>
-            <span className="spanHeader">Mi perfil</span>
-          </button>
+          <Link to={'/user/profile'}>
+            <button className="menu-button-user" onClick={toggleMenuUsuario}>
+              <span className="spanHeader">Mi perfil</span>
+            </button>
           </Link>
           <div className="separator-user-menu"></div>
           <button className="menu-button-user" onClick={handleLogout}>
@@ -116,29 +167,28 @@ function Encabezado() {
         </div>
       )}
 
-      {/* Tamaños de pantalla más pequeños */}
       {menuOpen && (
         <div className="menu-dropdown">
-          <Link to={"/"}>
-          <button className="menu-button">
-            <center>
-            <strong>Inicio</strong>
-            </center>
-          </button>
+          <Link to={'/'}>
+            <button className="menu-button">
+              <center>
+                <strong>Inicio</strong>
+              </center>
+            </button>
           </Link>
-          <Link to={"/About"}>
-          <button className="menu-button">
-            <center>
-            <strong>Sobre nosotros</strong>
-            </center>
-          </button>
+          <Link to={'/About'}>
+            <button className="menu-button">
+              <center>
+                <strong>Sobre nosotros</strong>
+              </center>
+            </button>
           </Link>
-          <Link to={"/Apoyanos"}>
-          <button className="menu-button">
-            <center>
-            <strong>Apóyanos</strong>
-            </center>
-          </button>
+          <Link to={'/Apoyanos'}>
+            <button className="menu-button">
+              <center>
+                <strong>Apóyanos</strong>
+              </center>
+            </button>
           </Link>
         </div>
       )}
