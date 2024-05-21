8fd01fa1ddfc7636cf467d53807182a26ee8918e
CamiloArdila <anardilad@unal.edu.co>
2024-05-21 11:16:41 -0500

    Correccion texto donacion

--- Frontend/src/pages/SupportUs.jsx
+++ Frontend/src/pages/SupportUs.jsx
@@ -60,7 +60,7 @@ function SupportUs() {
                     Sin embargo, mantener esta plataforma en funcionamiento no es tarea fácil. Aunque nos esforzamos por ofrecer nuestros servicios de forma gratuita para todos, también dependemos de ingresos para cubrir los costos de mantenimiento del sitio, el desarrollo de nuevas características y la expansión de nuestra comunidad. Es por eso que las donaciones de nuestros usuarios son vitales para nuestro éxito continuo.
                     </p>
                     <p className="product-description">
-                    Cada contribución que recibimos nos ayuda a seguir mejorando y creciendo, asegurando que podamos continuar ofreciendo un espacio enriquecedor y acogedor para todos los amantes de la cocina. Apreciamos profundamente el apoyo de nuestra comunidad y estamos comprometidos a seguir sirviéndoles con excelencia en los años venideros. Si deseas apoyarnos, ¡te lo agradecemos de todo corazón!. El monto minimo es de $5000 pesos Colombianos, y puedes hacerlo a continuación:
+                    Cada contribución que recibimos nos ayuda a seguir mejorando y creciendo, asegurando que podamos continuar ofreciendo un espacio enriquecedor y acogedor para todos los amantes de la cocina. Apreciamos profundamente el apoyo de nuestra comunidad y estamos comprometidos a seguir sirviéndoles con excelencia en los años venideros. Si deseas apoyarnos, ¡te lo agradecemos de todo corazón!
                     </p>
                     <p className="product-description">
                     El monto minimo es de $5000 pesos Colombianos, tenemos disponibles todos los medios de pago (Tarjetas de crédito, débito, Nequi, Daviplata, PSE y efectivo) y puedes hacerlo a continuación:
